ed78d1042ed7d95c36c74679b34c90838a8cba49
mrpis2 <warhammerprogamer@gmail.com>
2023-12-15 21:55:06 +0700

    print shapes

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= 5; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 5; j > i; j--)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine("");
+
+        }
+    }
+}
